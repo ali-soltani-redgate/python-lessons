a4bf6f47d379f83bcdd6f8d7962e0de573e2eda8
Ali Soltani <ali.soltani@red-gate.com>
2025-11-16 15:32:44 +0000

    Add polymorphism

---
+++ functions/polymorphism.py
@@ -0,0 +1,9 @@
+
+def times(x, y):       # Create and assign function
+    return x * y
+
+result = times(3, 4)  # Call function
+print(result)         # Output: 12
+
+result = times("Hello", 3)  # Call function with string and int
+print(result)               # Output: HelloHelloHello
